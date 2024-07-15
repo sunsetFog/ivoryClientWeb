e6e0694673825f3604a255d8369702303d69bfab
ivory <jsivory@itcom666.com>
2024-07-15 15:29:25 +0800

    2024-7-15 15:30

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -2,7 +2,22 @@ import React from 'react';
 import styles from './index.modules.scss';
 
 function NotFound() {
-    return <div className={styles['not-found-root']}>404</div>;
+    return (
+        <div className={styles['not-found-root']}>
+            <aside className={styles.yummy}></aside>
+            <main className={styles.delicious}>
+                <div className={styles.sticky}>没找到这个页面</div>
+                <div className={styles.tender}>PAGE NOT FOUND</div>
+                <div className={styles.refreshing}>
+                    尊敬的用户，很抱歉，此页面未找到，请您联系客服为您提供解决方案，对您造成的不便，我们深表歉意，感谢您的理解与支持。
+                </div>
+                <div className={styles.zippy}>
+                    <button>联系客服</button>
+                    <button>前往首页</button>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default NotFound;
