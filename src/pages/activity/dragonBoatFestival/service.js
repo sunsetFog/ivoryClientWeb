b9e0773c20be76b61ab272a5fbe835428d34fd91
ivory <1456300078@qq.com>
2025-11-14 07:38:15 +0800

    2025-11-14 7:38

--- src/pages/activity/dragonBoatFestival/service.js
+++ src/pages/activity/dragonBoatFestival/service.js
@@ -37,7 +37,8 @@
 
 // import customAxios from '~/api/http';
 import { get, post } from '@/@energy/ivoryDesign/@http/fetch';
-let domain = 'http://localhost:8062/sky';
+// let domain = 'http://localhost:8062/sky';
+let domain = 'http://8.148.240.171:9091/sky';
 export function getActiveInfoReq() {
     return get(domain + '/v3/activities', {});
 }
